818daaeea5549397564efe1fa13952b11b520af5
kakeru1215 <enshou01@gmail.com>
2015-10-02 01:48:47 +0900

    table9x9.java

---
+++ table9x9.java
@@ -0,0 +1,18 @@
+//打印9x9乘法表 
+public class table9x9 {
+	public static void main(String[] args){
+		for(int y=0;y<=9;y++){
+			for(int x=0;x<=9;x++){
+				if(x==0&&y==0){
+					System.out.print("0	1	2	3	4	5	6	7	8	9");
+					break;
+				}else if(x==0){
+					System.out.print(y + "\t");
+				}else{
+					System.out.print(x*y + "\t");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
