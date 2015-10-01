e75e120c9c6eb663e6895129a0b8e38b650ae1d3
kakeru1215 <enshou01@gmail.com>
2015-10-02 01:46:21 +0900

    forTest01.java
    draw some pattern by FOR

---
+++ forTest01.java
@@ -0,0 +1,37 @@
+import java.io.*;
+//for语句生成一些简单字符串团
+public class forTest01 {
+	public static void main(String[] args) throws IOException{
+	//practice01
+	for(int i=0;i<=5;i++){
+		for(int w=0;w<=5;w++){
+			System.out.print("@");
+		}
+		System.out.println();
+	}
+	System.out.println();
+	//practice02
+	for(int i=0;i<=5;i++){
+		for(int w=0;w<=5;w++){
+			if((w>0&&w<5)&&(i>0&&i<5)){
+				System.out.print("@");
+			}else{
+					System.out.print("X");
+			}
+		}
+		System.out.println();
+	}
+	System.out.println();
+	//practice03
+	for(int i=0;i<=5;i++){
+		for(int w=0;w<=5;w++){
+			if(w>=i){
+				System.out.print("@");
+			}else{
+					System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}	
+	}
+}
