e055a82089f4dd9faa761d77b09b3faa80c76478
kakeru1215 <enshou01@gmail.com>
2015-10-02 01:51:33 +0900

    zhishu.java

---
+++ zhishu.java
@@ -0,0 +1,19 @@
+public class zhishu{
+		public static void main(String[] args){
+			int count = 0;
+			for( int A = 2;A <= 100;A++ ){
+				boolean flag = false;
+				for( int B = 2;B < A;B++ ){
+					if(A%B == 0){
+					flag = true;
+					}
+				}
+				if(flag == false){
+					count++;
+					System.out.printf("%2d 是质数\n",A);
+				}
+			}
+		System.out.println("==================");
+		System.out.println("质数的个数是 : "+count);
+		}
+}
